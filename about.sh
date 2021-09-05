c6b0d0cd022aca4b7275a363fa08c36ac153fd8f
Ianqitgu742543 <87284724+Ianqitgu742543@users.noreply.github.com>
2021-09-05 21:56:41 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+clear
+echo -e "=================================================" | lolcat
+echo -e "#     Premium Auto Script By SSH Sedang         #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" 
+echo -e "# For VPS with KVM and VMWare virtualization    #" 
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# Thanks To  :                                  #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# ALLAH S.W.T                                   #" 
+echo -e "# My Family                                     #" 
+echo -e "# Google                                        #" 
+echo -e "# Youtube                                        #" 
+echo -e "# Jhon Arjuna 1335                      #" 
+echo -e "# Faisal                                       #" 
+echo -e "# Dimas Dm                                    #"
+echo -e "# Sanzking                                        #" 
+echo -e "# Kewek                                        #" 
+echo -e "#                                               #" 
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# Rebuild By : ADIT GABROK404               #" 
+echo -e "# Contact    : WhatsApp               #" 
+echo -e "# Whatsapp   : 085331489943                     #" 
+echo -e "=================================================" | lolcat
